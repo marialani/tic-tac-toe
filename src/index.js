a6a72b5ca40f6b8b599ef3c0447b94e2adbce9a7
Maria <m.olanipekun@hotmail.com>
2020-03-05 08:30:05 +0000

    modularise game component

--- src/index.js
+++ src/index.js
@@ -2,26 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
   
-  class Game extends React.Component {
-    render() {
-      return (
-        <div className="game">
-          <div className="game-board">
-            <Board />
-          </div>
-          <div className="game-info">
-            <div>{/* status */}</div>
-            <ol>{/* TODO */}</ol>
-          </div>
-        </div>
-      );
-    }
-  }
-  
-  // ========================================
-  
-  ReactDOM.render(
-    <Game />,
-    document.getElementById('root')
-  );
+ReactDOM.render(<Game />, document.getElementById('root'));
   
