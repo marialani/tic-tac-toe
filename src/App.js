d2bdabf3031a7971b285a5dd2172020fcb79ce48
Maria <m.olanipekun@hotmail.com>
2020-03-05 08:31:25 +0000

    add game component to app.js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import Game from './components/Game';
 export class App extends Component {
     render() {
         return (
             <div>
-                
+                <Game />
             </div>
         )
     }
